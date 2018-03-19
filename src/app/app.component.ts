80cf64b740ec5ee4d97134ab59727ad39e993392
Eyad Farra <eyadm.fa@gmail.com>
2018-03-19 18:04:45 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ElectronService} from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'app';
+
+  constructor(private _electronService: ElectronService) {
+
+  }
+
+  lunchWindow() {
+    this._electronService.shell.openExternal('https://www.facebook.com/Eyafa');
+  }
 }
