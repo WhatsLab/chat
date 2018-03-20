d8bd37427e74827f18422514e55636c7ae9cb890
Eyad Farra <eyadm.fa@gmail.com>
2018-03-20 18:28:14 +0200

    construct routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,74 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule, MatMenuModule} from '@angular/material';
 import {NgxElectronModule} from 'ngx-electron';
 
-import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
 
+import {AppComponent} from './app.component';
+import {MainComponent} from './main/main.component';
+import { BlankComponent } from './blank/blank.component';
+import { ChatComponent } from './chat/chat.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent,
+    data: {
+      title: 'Heroes List'
+    },
+    children: [
+      {
+        path: 'blank',
+        component: BlankComponent
+      },
+      {
+        path: 'chat',
+        component: ChatComponent
+      },
+      {
+        path: '',
+        redirectTo: '/main/blank',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    BlankComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     NgxElectronModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatIconModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
